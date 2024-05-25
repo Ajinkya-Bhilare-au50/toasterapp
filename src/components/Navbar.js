582a4d65e0ccea636c9e28a0639a916dee09128f
Ajinkya Bhilare <ajinkya0011@gmail.com>
2024-05-25 08:46:21 +0530

    updates4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
-// Navbar.js
-import React from "react";
-import { FaLinkedin } from "react-icons/fa6";
-import { Link, Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { VscGithub } from "react-icons/vsc";
+
 const Navbar = () => {
+  const location = useLocation();
+  const [selected, setSelected] = useState(location.pathname);
+
+  const handleLinkClick = (path) => {
+    setSelected(path);
+  };
+
   return (
     <div className="Navbar">
       <div className="Header">Header</div>
@@ -13,20 +20,32 @@ const Navbar = () => {
           href="https://github.com/Ajinkya-Bhilare-au50/toasterapp/tree/master"
           target="_blank"
         >
-          <VscGithub size={34} color="white" />
+          <VscGithub className="icon" size={34} color="white" />
         </a>
         <a rel="noreferrer" href="https://www.linkedin.com/in/ajinkya-bhilare/">
-          <FaLinkedin size={34} color="white" />
+          <FaLinkedin className="icon" size={34} color="white" />
         </a>
       </div>
       <div className="MainContent">
-        <Link to="/component1" className="Component1">
+        <Link
+          to="/component1"
+          className={selected === "/component1" ? "selected" : ""}
+          onClick={() => handleLinkClick("/component1")}
+        >
           Component1
         </Link>
-        <Link to="/component2" className="Component2">
+        <Link
+          to="/component2"
+          className={selected === "/component2" ? "selected" : ""}
+          onClick={() => handleLinkClick("/component2")}
+        >
           Component2
         </Link>
-        <Link to="/component3" className="Component3">
+        <Link
+          to="/component3"
+          className={selected === "/component3" ? "selected" : ""}
+          onClick={() => handleLinkClick("/component3")}
+        >
           Component3
         </Link>
       </div>
